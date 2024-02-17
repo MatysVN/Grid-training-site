efcf7ebe08951760fee4f4070f48f4f48d01ab5d
Mateusz <mateuszskc@gmail.com>
2024-02-17 20:30:00 +0100

    complete mobile nav (write javascript and styled in css) and styled header for large desktops

---
+++ js/style.js
@@ -0,0 +1,30 @@
+const burgerBtn = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav-mobile");
+const navItem = document.querySelectorAll(".nav__link");
+const body = document.querySelector('body');
+const footerYear = document.querySelector(".footer__year");
+
+const showNav = () => {
+    burgerBtn.classList.toggle('is-active');
+    nav.classList.toggle('nav-mobile--active');
+    body.classList.toggle('hidden');
+
+    navItem.forEach((item) => {
+		item.addEventListener("click", () => {
+            burgerBtn.classList.remove('is-active');
+            nav.classList.remove('nav-mobile--active');
+		});
+	});
+};
+
+
+const currentYear = () => {
+	const year = (new Date).getFullYear();
+	footerYear.innerText = year;
+};
+
+currentYear();
+
+burgerBtn.addEventListener("click", showNav);
+navItem.addEventListener('click', showNav);
+
