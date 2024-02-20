4cd64809a738283abedf0a452f5f010d552f99c8
Mateusz <mateuszskc@gmail.com>
2024-02-20 20:22:52 +0100

    created and styled sections > contact and gallery, correct javascript because when i click javascript because after clicking on the burger menu, the photos did not disappear, and after clicking on the section name, it did not remove the hidden class from the body

--- js/style.js
+++ js/style.js
@@ -1,30 +1,29 @@
 const burgerBtn = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav-mobile");
 const navItem = document.querySelectorAll(".nav__link");
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 const footerYear = document.querySelector(".footer__year");
 
 const showNav = () => {
-    burgerBtn.classList.toggle('is-active');
-    nav.classList.toggle('nav-mobile--active');
-    body.classList.toggle('hidden');
+	burgerBtn.classList.toggle("is-active");
+	nav.classList.toggle("nav-mobile--active");
+	body.classList.toggle("hidden");
 
-    navItem.forEach((item) => {
+	navItem.forEach((item) => {
 		item.addEventListener("click", () => {
-            burgerBtn.classList.remove('is-active');
-            nav.classList.remove('nav-mobile--active');
+			burgerBtn.classList.remove("is-active");
+			body.classList.remove("hidden");
+			nav.classList.remove("nav-mobile--active");
 		});
 	});
 };
 
-
 const currentYear = () => {
-	const year = (new Date).getFullYear();
+	const year = new Date().getFullYear();
 	footerYear.innerText = year;
 };
 
 currentYear();
 
 burgerBtn.addEventListener("click", showNav);
-navItem.addEventListener('click', showNav);
-
+navItem.addEventListener("click", showNav);
